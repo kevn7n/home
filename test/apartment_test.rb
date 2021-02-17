2410ce48ebfef2305c2588e931edd96052c68af5
kevn7n <kevn_nguyen@yahoo.com>
2021-02-17 09:27:55 -0700

    Complete It.1 renter & apt class

--- test/apartment_test.rb
+++ test/apartment_test.rb
@@ -5,13 +5,18 @@ require './lib/renter'
 
 class ApartmentTest < Minitest::Test
   def setup
-    @unit1 = Apartment.new({number: "A1"}, monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    @unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    @renter1 = Renter.new("Jessie")
 
   end
 
   def test_it_exists
 
-    assert_instance_of Apartment, @unit1.number
+    assert_instance_of Apartment, @unit1
+  end
+
+  def test_it_has_unit_number
+    assert_equal "A1", @unit1.number
   end
 
   def test_it_has_monthly_rent
@@ -27,8 +32,8 @@ class ApartmentTest < Minitest::Test
   def test_it_has_a_renter
     assert_equal nil, @unit1.renter
 
-    @unit.add_renter(renter1)
+    @unit1.add_renter(@renter1)
 
-    assert_equal renter1, @unit1.renter
+    assert_equal @renter1, @unit1.renter
   end
 end
